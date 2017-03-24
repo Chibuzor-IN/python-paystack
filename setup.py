d69541b6a9dc5cbbab789c13755fc5590fb6ed0b
Chibuzor-IN <nwalorc@gmail.com>
2017-03-24 23:41:35 +0100

    Minor fixes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name = 'python_paystack',
-      version = 0.5,
+      version = 0.6,
       description = 'A Paystack API wrapper',
       url = '',
       author = 'Nwalor Chibuzor',
