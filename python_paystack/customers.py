1097b9f46074b41379bec367e41ab861d286636c
Chibuzor-IN <nwalorc@gmail.com>
2017-03-25 10:52:40 +0100

    Minor fixes

--- python_paystack/customers.py
+++ python_paystack/customers.py
@@ -18,7 +18,7 @@ class Customer():
     def __init__(self, email, first_name = None, last_name = None, phone = None, risk_action = None, id = None):
         if validators.email(email):
             self.email = email
-            self.first_name = firs_name
+            self.first_name = first_name
             self.last_name = last_name
             self.phone = phone
             self.risk_action = risk_action
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name = 'python_paystack',
-      version = 0.8,
+      version = 0.9,
       description = 'A Paystack API wrapper',
       url = '',
       author = 'Nwalor Chibuzor',
