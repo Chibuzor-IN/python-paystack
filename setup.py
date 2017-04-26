fdaccef518adc867684f66f45dabf5e224bf316f
Chibuzor-IN <nwalorc@gmail.com>
2017-04-26 20:02:04 +0100

    Increased version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='python_paystack',
-      version='1.0.6',
+      version='1.0.7',
       description='A Paystack API wrapper',
       url='',
       author='Nwalor Chibuzor',
